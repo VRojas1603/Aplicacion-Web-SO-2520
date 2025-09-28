4940940558fd764a094ab7d9a860d8ed3350a828
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-09-28 15:08:34 -0500

    feat(authentication): implement interceptor and guard services.

---
+++ src/app/iam/infrastructure/authentication-guard.ts
@@ -0,0 +1,21 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+
+import {map, take} from "rxjs";
+import {AuthenticationService} from './authentication';
+
+export const authenticationGuard: CanActivateFn = () => {
+  const authenticationService = inject(AuthenticationService);
+  const router = inject(Router);
+
+  return authenticationService.isSignedIn.pipe(
+    take(1), map(isSignedIn => {
+      if (isSignedIn)
+        return true;
+      else {
+        router.navigate(['/sign-in']).then();
+        return false;
+      }
+    })
+  );
+};
