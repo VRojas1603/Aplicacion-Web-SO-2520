01b74cf18c49a3c6a75555f084acc42a33202ac2
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-09-28 15:42:27 -0500

    chore: update project configuration with new dependencies and specify providers.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+
+import { routes } from './app.routes';
+
+import { provideAppInitializer, inject } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { withInterceptors } from "@angular/common/http";
+import { authenticationInterceptor } from "./iam/infrastructure/authentication-interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({prefix: './assets/i18n/', suffix: '.json'}),
+      lang: 'en',
+      fallbackLang: 'en'
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      translate.use(translate.getBrowserLang() || "en");
+    })
+  ]
 };
