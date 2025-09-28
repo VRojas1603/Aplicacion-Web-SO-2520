69d36fac2d7e09affcf0f6ece2eb9fc9cfc0225a
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-09-28 15:15:06 -0500

    feat(sign-in): create component for sign-in option.

---
+++ src/app/iam/presentation/components/sign-in/sign-in.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthenticationService} from "../../../infrastructure/authentication";
+import {BaseFormComponent} from "../../../../shared/presentation/components/base-form.component";
+import {SignInRequest} from "../../../domain/model/sign-in.request";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatError, MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatButton,
+    MatCardTitle,
+    MatError,
+  ],
+  templateUrl: './sign-in.html',
+  styleUrl: './sign-in.css'
+})
+export class SignInComponent extends BaseFormComponent implements OnInit {
+
+  form!: FormGroup;
+  submitted = false;
+
+  constructor(private builder: FormBuilder, private authenticationService: AuthenticationService) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.form = this.builder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid)
+      return;
+    let username = this.form.value.username;
+    let password = this.form.value.password;
+    const signInRequest = new SignInRequest(username, password);
+    this.authenticationService.signIn(signInRequest);
+    this.submitted = true;
+  }
+}
